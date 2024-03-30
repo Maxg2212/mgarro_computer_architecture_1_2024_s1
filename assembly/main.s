0a01e49b1549678efb7301aeeab452bc5aba5e96
Naheem Johnson <naheemjohnson@estudianTEC.cr>
2024-03-30 15:00:46 -0600

    Cambios en main.s, multi.s

--- assembly/main.s
+++ assembly/main.s
@@ -5,6 +5,8 @@
   constante_1_alpha: .word 0x00006666 @ Valor de 1 - alpha
   constante_alpha: .word 0x0000999a @ Valor de alpha
   constante_k: .word 20205 @ Resultado de realizar K = Fs x 50ms = 20205
+  constante_mask: .word 0xFFFF @ Máscara para obtener los 16 bits menos significativos
+  constante_loop: .word 2 @ Resultado de realizar K = Fs x 50ms = 20205
   name_input: .asciz "input.bin"
   name_output: .asciz "output.bin"  
   buffer_input: .space 1000000    @ reserved buffer
@@ -31,11 +33,12 @@ _start:
   
   
 _reverb:
-  ldr r2, [r1, #4] @ Cargo el valor al que apunta la direccion r1
+  ldr r2, [r1] @ Cargo el valor al que apunta la dirección r1
+  add r1, r1, #4 @ Incremento la dirección en 4 para apuntar al siguiente valor
  
   ldr r5, =constante_1_alpha
   ldr r6, [r5] @ (1 - alpha)
-
+  
   b _mult_pfijo1 @ (1 - 0.6) * x(n)
 
   _mult_end:
@@ -65,21 +68,30 @@ _reverb:
   @r10, r2 registros con valores
   add r12, r2, r9 @ (1 - 0.6) * x(n) + alpha * y(n-k)
 
-  str r12, [r0, #4] @ se almacena la posicion y(n) en la lista
+  _result_r12:
+  str r12, [r0] @ se almacena la posicion y(n) en la lista
+  add r0, r0, #4 @ Incremento la dirección en 4 para apuntar al siguiente valor
 
-  add r3, r3, #1  @ Incrementar el valor de r6 en uno
+  
 
-  cmp r3, #882298     @ comparar r3 con 882298 que es el tamaño completo del buffer
+  ldr r5, =constante_loop
+  ldr r8, [r5] @ constante de comparacion del loop
+
+  add r3, r3, #1  @ Incrementar el valor de r3 en uno
+
+  cmp r3, r8     @ comparar r3 con 882298 que es el tamaño completo del buffer
   blt _reverb         @ Repetir el ciclo si el valor en r3 es menor que el valor dado
   beq _loadfiles
 
 _mult_pfijo2:
-  @ r1, r0 reserved
+  ldr r5, =constante_mask
+  ldr r12, [r5] @ mask
+
   asr r7, r8, #16 @ Qa
   asr r5, r4, #16 @ Qb
 
-  and r10, r8, #0xFFFF @ Qc
-  and r11, r4, #0xFFFF @ Qd
+  and r10, r8, r12 @ Qc
+  and r11, r4, r12 @ Qd
 
   mul r12, r7, r5 @ high = a * b @ high =  a * c
   mul r6, r10, r11 @ low =  b * d @ low = b * d
@@ -92,15 +104,18 @@ _mult_pfijo2:
   lsr r6, r6, #16 @ low >> 16
 
   add r8, r12, r6 @ high<<<16 + low>>>16
-  add r9, r8, r7   @ high<<<16 + low>>>16 + mid (aun no se está utilizando buffer output)
+  add r9, r8, r5   @ high<<<16 + low>>>16 + mid (aun no se está utilizando buffer output)
   b _mult_end2
 
 _mult_pfijo1:
+  ldr r5, =constante_mask
+  ldr r8, [r5] @ (1 - alpha)
+
   asr r4, r6, #16 @ Qa
   asr r7, r2, #16 @ Qb
 
-  and r5, r6, #0xFFFF @ Qc
-  and r10, r2, #0xFFFF @ Qd
+  and r5, r6, r8 @ Qc
+  and r10, r2, r8 @ Qd
 
   mul r11, r4, r7 @ high = a * b 
   mul r12, r5, r10 @ low =  c * d 
@@ -129,17 +144,17 @@ _greater_than:
   b _end_if_else     @ Salto al final de la sección _reverb
 _loadfiles:
    @ Load the outbit.bin file
-   @mov r7, #0x5            
-   @ldr r2, =name_output   
-   @mov r1, #2         
-   @mov r2, #100
-   @swi 0          
+   mov r7, #0x5            
+   ldr r2, =name_output   
+   mov r1, #2         
+   mov r2, #100
+   swi 0          
 
   @ Writes the output buffer in output.bin file
-   @mov r7, #0x4
-   @ldr r3, =buffer_output
-   @ldr r2, =#882298
-   @swi 0
+   mov r7, #0x4
+   ldr r3, =buffer_output
+   ldr r2, =#8
+   swi 0
 
 _end:
   @ Close input
@@ -147,8 +162,8 @@ _end:
   swi 0                   
 
   @ @ Close output
-   @mov r7, #6             
-   @swi 0                  
+   mov r7, #6             
+   swi 0                  
 
   @ Finish program
   mov r7, #0x1            
