795bec219ab621a24adc0f65fb03e3576eeea388
Naheem Johnson <naheemjohnson@estudianTEC.cr>
2024-03-30 00:45:40 -0600

    Algoritmo avanzado (faltan pruebas)

--- assembly/main.s
+++ assembly/main.s
@@ -2,10 +2,9 @@
 
 
 .section .data
-  constante_1: .word 0x10000 @ Valor de 1 en formato q16.17
-  constante_alpha: .word 0xFFFF6666 @ Valor de alpha = 0.6 en formato Q16.17
+  constante_1_alpha: .word 0x00006666 @ Valor de 1 - alpha
+  constante_alpha: .word 0x0000999a @ Valor de alpha
   constante_k: .word 20205 @ Resultado de realizar K = Fs x 50ms = 20205
-  mascara: .word 0xFFFF
   name_input: .asciz "input.bin"
   name_output: .asciz "output.bin"  
   buffer_input: .space 1000000    @ reserved buffer
@@ -19,66 +18,106 @@ _start:
   mov r1, #2              
   mov r2, #0              
   swi 0                   
-  @ R1 = buffer, R2 = buffer size, R3 = constant1, R4 = alpha, R5 = constantk, R6 - R9 values of the constants, R7 = swi, R10 - R12 not used
-
 
   @ Read the file with buffer
   mov r7, #0x3            
   ldr r1, =buffer_input   
   ldr r2, =#882298       @ buffer size
-  swi 0   
-
-  @ldr r1, =buffer_input
-  ldr r2, =buffer_output
-
-  @ Load the first 32-bit number into r8 ( x(1) )
+  swi 0     
   
-  
-  @ Load the addresses of the different constants
-  ldr r3, =constante_1
-  ldr r4, =constante_alpha
-  ldr r5, =constante_k
-
-  @ Load the values of the constants
-  ldr r6, [r3] @ 1 Q16.17
-  ldr r8, [r4] @ alpha q16.17
-  ldr r9, [r5] @ constant k
-
   mov r3, #0 @ main counter for loop
-
+  ldr r0, =buffer_output @ Cargar la dirección de buffer_output 
+  ldr r1, =buffer_input  @ Cargar la dirección de buffer_input 
+  
+  
 _reverb:
+  ldr r2, [r1, #4] @ Cargo el valor al que apunta la direccion r1
+ 
+  ldr r5, =constante_1_alpha
+  ldr r6, [r5] @ (1 - alpha)
 
-  ldr r8, [r1]            @ load first 32 bits into r8 x(n)
+  b _mult_pfijo1 @ (1 - 0.6) * x(n)
 
-  @(1 - a)x(n)
-  add r10, r6, r8  @ 1 - 0.6 result  
-  mul r11, r10, r1 @ (1 - 0.6) * x(n) (se debe cambiar como se realiza la multiplicacion)
+  _mult_end:
+    @ Se almacena en r2 resultado (1 - 0.6) * x(n)
+
+  ldr r5, =constante_k @ Cargar la dirección de constante_k en r5
+  ldr r9, [r5] @ constant k @ Cargar el valor de constante_k en r9
 
   sub r10, r3, r9 @ (n - k)
+
 
   cmp r10, #0        @ Compara el valor en r10 con cero
   beq _equals   @ Salta a valor_igual_cero si r10 es igual a cero
   blt _less_than    @ Salta a valor_menor si r10 es menor que cero
   bgt _greater_than    @ Salta a valor_mayor si r10 es mayor que cero
 
-  
   _end_if_else:
-  
-  mul r8, r4 @ alpha * y(n-k)
+    @ Se almacena en r4 el valor de la lista y(n-k)
 
-  add r12, r11, r8 @ (1 - 0.6) * x(n) + alpha * y(n-k)
+  ldr r5, =constante_alpha
+  ldr r8, [r5] @ alpha q16.17
+  b _mult_pfijo2 @ alpha * y(n-k)
 
-  str r12, [r2] @ se almacena la posicion y(n) en la lista
+  _mult_end2:
+    @ Se almacena en r8 resultado alpha * y(n-k)
+
+  @r10, r2 registros con valores
+  add r12, r2, r9 @ (1 - 0.6) * x(n) + alpha * y(n-k)
+
+  str r12, [r0, #4] @ se almacena la posicion y(n) en la lista
 
   add r3, r3, #1  @ Incrementar el valor de r6 en uno
 
-  @cmp r3, #882298     @ comparar r3 con 882298 que es el tamaño completo del buffer
-  @blt _reverb         @ Repetir el ciclo si el valor en r3 es menor que el valor dado
-  @beq _loadfiles
-_mult_pfijo:
+  cmp r3, #882298     @ comparar r3 con 882298 que es el tamaño completo del buffer
+  blt _reverb         @ Repetir el ciclo si el valor en r3 es menor que el valor dado
+  beq _loadfiles
+
+_mult_pfijo2:
+  @ r1, r0 reserved
+  asr r7, r8, #16 @ Qa
+  asr r5, r4, #16 @ Qb
+
+  and r10, r8, #0xFFFF @ Qc
+  and r11, r4, #0xFFFF @ Qd
+
+  mul r12, r7, r5 @ high = a * b @ high =  a * c
+  mul r6, r10, r11 @ low =  b * d @ low = b * d
+
+  mul r8, r7, r11 @ high = a * d 
+  mul r9, r10, r5 @ high = b * c
+  add r5, r8, r9 @ mid 
+
+  lsl r12, r12, #16 @ high << 16
+  lsr r6, r6, #16 @ low >> 16
+
+  add r8, r12, r6 @ high<<<16 + low>>>16
+  add r9, r8, r7   @ high<<<16 + low>>>16 + mid (aun no se está utilizando buffer output)
+  b _mult_end2
+
+_mult_pfijo1:
+  asr r4, r6, #16 @ Qa
+  asr r7, r2, #16 @ Qb
+
+  and r5, r6, #0xFFFF @ Qc
+  and r10, r2, #0xFFFF @ Qd
+
+  mul r11, r4, r7 @ high = a * b 
+  mul r12, r5, r10 @ low =  c * d 
+
+  mul r9, r4, r10 @ high = a * d 
+  mul r2, r5, r7 @ high = b * c
+  add r7, r9, r2 @ mid 
+
+  lsl r11, r11, #16 @ high << 16
+  lsr r12, r12, #16 @ low >> 16
+
+  add r4, r11, r12 @ high<<<16 + low>>>16
+  add r2, r4, r7   @ high<<<16 + low>>>16 + mid (aun no se está utilizando buffer output)
+  b _mult_end
 
 _equals:
-    ldr r4, [r2]
+    ldr r4, [r0]
     b _end_if_else    @ Salto al final de la sección _reverb
 _less_than:
   mov r4, #0
@@ -86,27 +125,19 @@ _less_than:
 _greater_than:
   mov r5, #4       @ Establecer r4 en cero si r10 es igual a cero
   mul r10, r5       @ multiplicamos el indice por 4 para poder acceder a la lista sin problemas
-  ldr r4, [r2, r10]
+  ldr r4, [r0, r10]
   b _end_if_else     @ Salto al final de la sección _reverb
 _loadfiles:
-  @ Cargar el valor #5 en la primera posición de buffer_output
-  @ldr r1, =buffer_output     @ Cargar la dirección de buffer_output en r1
-  @mov r3, #5                 @ Cargar el valor #5 en r3
-  @str r3, [r1]               @ Guardar el valor #5 en la primera posición de buffer_output
-
-  @ldr r4, [r1]
-
-
    @ Load the outbit.bin file
    @mov r7, #0x5            
-   @ldr r0, =name_output   
+   @ldr r2, =name_output   
    @mov r1, #2         
    @mov r2, #100
    @swi 0          
 
   @ Writes the output buffer in output.bin file
    @mov r7, #0x4
-   @ldr r1, =buffer_output
+   @ldr r3, =buffer_output
    @ldr r2, =#882298
    @swi 0
 
