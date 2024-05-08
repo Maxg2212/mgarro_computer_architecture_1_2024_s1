4cc162a58cb4373a24c5346cbae288315cd9aeff
Max Garro Mora <maxgarro22@gmail.com>
2024-05-07 23:55:58 -0600

    [Update] Alu

---
+++ MemoyModules/alu_defs.sv
@@ -0,0 +1,11 @@
+package alu_defs;
+
+// Operaciones de la ALU
+
+		parameter ARITH_SUM = 3'b000;
+		parameter ARITH_RES = 3'b001;
+		parameter ARITH_MUL = 3'b010;
+		parameter LOGIC_DLD = 3'b011;
+		parameter CR_ = 3'b100;
+		
+endpackage 
